b9304153a54bd92cc81425500a81d277c49c78b6
Meet Bhimani <bhimanimeet55555@gmail.com>
2023-06-07 21:04:15 +0530

    updates for practice

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const hbs = require('hbs')
+const express = require('express');
+const app = express();
+const path = require('path');
+const hbs = require('hbs');
 const PORT = process.env.PORT || 3000
 
 app.set('view engine', 'hbs')
